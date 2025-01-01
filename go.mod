f114768e2fde1477af7a90689aacb365ae63e906
Will Chen <will.chen936@gmail.com>
2025-01-02 00:15:23 +0800

    fix: Utils & Tests

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.2
 require (
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.10.0
+	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67
 )
 
 require (
--- utils/random.go
+++ utils/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"strings"
 	"time"
 
 	"golang.org/x/exp/rand"
@@ -9,17 +10,17 @@ import (
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(uint64(time.Now().UnixNano()))
 }
 
-func RandomInt(min, max int64) {
+func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
 }
 
-func RandomString(length int) {
-	var sb string.builder
+func RandomString(length int) string {
+	var sb strings.Builder
 	k := len(alphabet)
-	for i = 0; i < length; i++ {
+	for i := 0; i < length; i++ {
 		c := alphabet[rand.Intn(k)]
 		sb.WriteByte(c)
 	}

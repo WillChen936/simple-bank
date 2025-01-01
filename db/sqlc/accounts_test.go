d5e528762c54f7d9585637787ffb1f8ee1c5758b
Will Chen <will.chen936@gmail.com>
2025-01-02 01:10:12 +0800

    test: Add Test for Account

--- db/sqlc/accounts_test.go
+++ db/sqlc/accounts_test.go
@@ -3,10 +3,36 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/WillChen936/simple-bank/utils"
 	"github.com/stretchr/testify/require"
 )
+
+func TestCreateAccount(t *testing.T) {
+	account := createRandomAccount(t)
+	testQueries.DeleteAccount(context.Background(), account.ID)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+// func TestDeleteAccount(t *testing.T) {
+// 	account := createRandomAccount(t)
+// 	err := testQueries.DeleteAccount(context.Background(), account.ID)
+
+// 	require.NoError(t, err)
+// }
 
 func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
@@ -14,7 +40,6 @@ func createRandomAccount(t *testing.T) Account {
 		Balance:  utils.CreateRandomMoney(),
 		Currency: utils.CreateRandomCurrency(),
 	}
-
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
@@ -23,8 +48,4 @@ func createRandomAccount(t *testing.T) Account {
 	require.Equal(t, account.Currency, arg.Currency)
 
 	return account
-}
-
-func TestCreateAccount(t *testing.T) {
-	createRandomAccount(t)
 }

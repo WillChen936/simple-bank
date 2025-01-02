c75315c363fdea81ffbece2994344bd81b655657
Will Chen <will.chen936@gmail.com>
2025-01-02 19:11:31 +0800

    feat: Add api server

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	db "github.com/WillChen936/simple-bank/db/sqlc"
+	"github.com/WillChen936/simple-bank/utils"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	config  utils.Config
+	queries *db.Queries
+	router  *gin.Engine
+}
+
+func NewServer(config utils.Config, queries *db.Queries) *Server {
+	server := &Server{
+		config:  config,
+		queries: queries,
+	}
+
+	server.SetupRouter()
+
+	return server
+}
+
+func (server *Server) SetupRouter() {
+	router := gin.Default()
+
+	server.router = router
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}

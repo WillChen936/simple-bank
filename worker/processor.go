0bc6a50b1e053fac31ad3023926fd2e5d89b58dd
Will Chen <will.chen936@gmail.com>
2025-08-05 00:00:00 +0800

    feat: Implement background worker with Redis task queue

---
+++ worker/processor.go
@@ -0,0 +1,38 @@
+package worker
+
+import (
+	"context"
+
+	db "github.com/WillChen936/simple-bank/db/sqlc"
+	"github.com/hibiken/asynq"
+)
+
+type TaskProccessor interface {
+	Start() error
+	ProccessTaskSendVerifyEmail(ctx context.Context, task *asynq.Task) error
+}
+
+type RedisTaskProccessor struct {
+	server *asynq.Server
+	store  db.Store
+}
+
+func NewRedisTaskProccessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProccessor {
+	server := asynq.NewServer(
+		redisOpt,
+		asynq.Config{},
+	)
+
+	return &RedisTaskProccessor{
+		server: server,
+		store:  store,
+	}
+}
+
+func (processor *RedisTaskProccessor) Start() error {
+	mux := asynq.NewServeMux()
+
+	mux.HandleFunc(TaskSendVerifyEmail, processor.ProccessTaskSendVerifyEmail)
+
+	return processor.server.Start(mux)
+}
